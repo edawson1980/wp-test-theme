da48a2f27a613a35104af4e4f74b3fe767a50d8d
Erin <erinedawson@gmail.com>
2019-08-22 14:36:42 -0600

    add header.php

---
+++ myTheme/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>EED Test Site</title>
+  </head>
+  <body>
+    <header>
+      <h1>Header goes here</h1>
+    </header>
